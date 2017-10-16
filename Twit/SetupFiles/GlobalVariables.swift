cd0e33f6fb4c9d41dccb1b55a10aa1e635f87bfe
Manraaj Nijjar <ManraajNijjar@gmail.com>
2017-10-16 18:44:04 -0400

    Setup: Created and organized Files for the Twitter Feed and the Twitter API controller

---
+++ Twit/SetupFiles/GlobalVariables.swift
@@ -0,0 +1,14 @@
+//
+//  GlobalVariables.swift
+//  Twit
+//
+//  Created by Manraaj Nijjar on 10/12/17.
+//  Copyright © 2017 Manraaj Nijjar. All rights reserved.
+//
+
+import Foundation
+
+class GlobalVariables {
+    var consumerKey = "0ffjb8hTtcdzIddl1d71CcZpi";
+    var consumerSecret = "jn0gTgAKpkxRGaN2d9Dmx5be8HTuwRDavy9H66ObBWP7YNfH4v";
+}
